4ea2da90abbaaf620a76a138dea4662e9de89067
Héctor Fornes <hctrfg@gmail.com>
2018-02-08 15:36:03 +0100

    Hardcode user authenticaton in apollo

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache } from 'apollo-client-preset';
 
+import { AUTH_TOKEN } from './constants.js';
 import configureStore from './configureStore.js';
 
 import Root from './components/Root.js';
@@ -9,8 +10,23 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
+    const httpLink = new HttpLink({ uri: 'https://jangala.herokuapp.com' });
+
+    const middlewareAuthLink = new ApolloLink((operation, forward) => {
+      const token = AUTH_TOKEN; // TODO: use asyncStorage
+      const authorizationHeader = token ? `Bearer ${token}` : null;
+      operation.setContext({
+        headers: {
+          authorization: authorizationHeader,
+        },
+      });
+      return forward(operation);
+    });
+
+    const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
     this.client = new ApolloClient({
-      link: new HttpLink({ uri: 'https://jangala.herokuapp.com' }),
+      link: httpLinkWithAuthToken,
       cache: new InMemoryCache()
     });
 
